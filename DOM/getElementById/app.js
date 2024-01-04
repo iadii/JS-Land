dccc87d4a33c3fc0bcd33d47fdac8427c5cc0015
Aditya mishra <mishraaditya3456@gmail.com>
2024-01-04 22:46:52 +0530

    Add files via upload

---
+++ DOM/getElementById/app.js
@@ -0,0 +1,9 @@
+// document.getElementsByClassName('bg-black').style.backgroundColor = "#C2847A"
+const tit = document.getElementById('title') 
+tit.style.backgroundColor = "#B1E5F2"
+tit.style.color = "#F686BD"
+
+const sec = document.getElementById('secHead')
+sec.innerHTML = "Hitesh Choudhary"
+
+document.getElementById('para').style.color = "#1F7A8C"
