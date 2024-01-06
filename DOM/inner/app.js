d04d113504425f087ee3d6199d5802916fac419a
Aditya mishra <mishraaditya3456@gmail.com>
2024-01-06 23:27:48 +0530

    Add files via upload

---
+++ DOM/inner/app.js
@@ -0,0 +1,40 @@
+const author = document.querySelector('h1 span')
+author.innerText = "Aditya Mishra";
+
+
+const techAdv = document.querySelector('h2')
+// size = h1 * h6
+const innHtm = "<h6> About Tech Advancement </h6>"
+// size = h1
+const innTex = "we are changing the size by innerHTML"
+// techAdv.innerText = "<h1>we are changing the size by innerHTML<h1>"
+techAdv.innerHTML = `${innHtm} ${innTex}`
+
+// since we have many h2s so we have to use querySelectorAll and 
+// give the desired indexing of h2 as querySelectorAll provides nodeList to manipulate the DOM
+
+const js = document.querySelectorAll('h2')
+js[1].textContent = 'Concept of JavaScript'
+
+const cpp = "Concept of  c++"
+const fav = '<h5>It is My Favorite language</h5>'
+
+js[2].innerHTML = `${cpp} ${fav}`
+
+// this will markup tag too
+console.log(document.querySelector('body').innerHTML);
+
+// This will Show only text
+console.log(document.querySelector('body').innerText);
+
+// giving id to h1
+document.querySelector('h1').id = 'whoops'
+
+const changeColor = document.querySelector('#whoops')
+changeColor.style.color = '#F2AF29'
+
+// Changing whole background Color
+const body = document.querySelector('body')
+
+body.style.backgroundColor = "#16001E"
+body.style.color = '#DADFF7'
