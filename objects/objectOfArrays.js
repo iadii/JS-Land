8367cd7a3385b0dc165f10bee237c26209576a33
Aditya mishra <mishraaditya3456@gmail.com>
2024-01-19 16:55:52 +0530

    Delete objects directory

--- objects/objectOfArrays.js
+++
@@ -1,8 +0,0 @@
-const obj = {
-    Sharda : ["Aditya" , 2022623669, 2201100099, "MCA"] ,
-    SGTU : ["Vats", 181345019, 181302005, "BCA"]
-}
-// accessing
-
-console.log(obj.Sharda[2]);
-console.log(obj["SGTU"][1 ]);
